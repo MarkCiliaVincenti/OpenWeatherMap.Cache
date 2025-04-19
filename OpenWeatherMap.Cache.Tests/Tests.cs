5212d002192ea453e3cb1fbcc1afa2aba7ed7af7
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2025-04-19 17:51:42 +0200

    Added tests

--- OpenWeatherMap.Cache.Tests/Tests.cs
+++ OpenWeatherMap.Cache.Tests/Tests.cs
@@ -1,5 +1,7 @@
 using Microsoft.Extensions.Configuration;
+using OpenWeatherMap.Cache.Models;
 using System;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -116,4 +118,83 @@ public class Tests
             Assert.Equal(i, totalFromAPI);
         }
     }
+
+    [Fact]
+    public async Task GetReadingsAsync_ShouldReturnSuccessfulReading_WhenNotCached()
+    {
+        var location = new Models.Location(37.7749, -122.4194);
+        var cache = new OpenWeatherMapCache(_apiKey, 1_000);
+
+        var result = await cache.GetReadingsAsync(location);
+
+        Assert.True(result.IsSuccessful);
+        Assert.True(result.ApiRequestMade);
+        Assert.False(result.IsFromCache);
+    }
+
+    [Fact]
+    public void GetReadings_ShouldReturnFromCache_WhenWithinCachePeriod()
+    {
+        var location = new Models.Location(40.7128, -74.0060);
+        var cache = new OpenWeatherMapCache(_apiKey, 10_000);
+
+        var first = cache.GetReadings(location);
+        var second = cache.GetReadings(location);
+
+        Assert.True(first.IsSuccessful);
+        Assert.True(second.IsFromCache);
+        Assert.False(second.ApiRequestMade);
+    }
+
+    public class InvalidQuery : ILocationQuery { }
+
+    [Fact]
+    public async Task GetReadingsAsync_ShouldThrowArgumentException_WhenInvalidLocationQuery()
+    {
+        var cache = new OpenWeatherMapCache(_apiKey, 1000);
+        await Assert.ThrowsAsync<ArgumentException>(async () => await cache.GetReadingsAsync(new InvalidQuery()));
+    }
+
+    [Fact]
+    public async Task GetReadingsAsync_ShouldLogResponse_WhenLogPathIsSet()
+    {
+        string tempDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+        Directory.CreateDirectory(tempDir);
+
+        var cache = new OpenWeatherMapCache(_apiKey, 1000, logPath: tempDir);
+        var location = new Models.Location(48.8566, 2.3522);
+
+        var result = await cache.GetReadingsAsync(location);
+
+        var expectedFile = Path.Combine(tempDir, $"{location.Latitude.ToString().Replace('.', '_')}-{location.Longitude.ToString().Replace('.', '_')}.json");
+        Assert.True(File.Exists(expectedFile));
+
+        // Cleanup
+        Directory.Delete(tempDir, true);
+    }
+
+    [Fact]
+    public void GetReadings_SynchronousMethod_ReturnsValidReading()
+    {
+        var cache = new OpenWeatherMapCache(_apiKey, apiCachePeriod: 1000);
+        var location = new Location(48.6371, -122.1237);
+
+        var reading = cache.GetReadings(location);
+
+        Assert.True(reading.IsSuccessful);
+        Assert.False(reading.IsFromCache);
+    }
+
+    [Fact]
+    public async Task GetReadingsAsync_ZipCodeQuery_ReturnsValidReading()
+    {
+        var cache = new OpenWeatherMapCache(_apiKey, apiCachePeriod: 1000);
+        var zipCode = new ZipCode("90210", "US");
+
+        var reading = await cache.GetReadingsAsync(zipCode);
+
+        Assert.True(reading.IsSuccessful);
+        Assert.False(reading.IsFromCache);
+    }
+
 }
